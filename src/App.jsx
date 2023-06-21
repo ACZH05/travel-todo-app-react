376c6f1ee14b9ef2a0d695151bef48e13e5680c8
Alfred Chin <alfredchin2005@gmail.com>
2023-06-21 13:30:49 +0800

    Add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+function Layout() {
+  return (
+    <>
+      <Navbar bg='light' variant='light'>
+        <Container>
+          <Navbar.Brand>Travel Plan App</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link>Checklist</Nav.Link>
+            <Nav.Link>Bucketlist</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Outlet />
+    </>
+  )
+}
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Outlet />}>
+        <Route path='/' element={<Layout />}>
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Register />} />
         </Route>
