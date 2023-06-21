e9e0889f94b62cee9b7c1ffaa20e605e59081b45
Alfred Chin <alfredchin2005@gmail.com>
2023-06-21 19:29:42 +0800

    Create bucketlist.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import Checklist from './pages/Checklist'
+import Bucketlist from './pages/Bucketlist'
 
 function Layout() {
   return (
@@ -11,8 +12,8 @@ function Layout() {
         <Container>
           <Navbar.Brand>Travel Plan App</Navbar.Brand>
           <Nav className='me-auto'>
-            <Nav.Link>Checklist</Nav.Link>
-            <Nav.Link>Bucketlist</Nav.Link>
+            <Nav.Link href='/checklist'>Checklist</Nav.Link>
+            <Nav.Link href='/bucketlist'>Bucket list</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -27,6 +28,7 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Layout />}>
           <Route index path='checklist' element={<Checklist />} />
+          <Route index path='bucketlist' element={<Bucketlist />} />
           <Route path='login' element={<Login />} />
           <Route path='register' element={<Register />} />
         </Route>
---
+++ src/pages/Bucketlist.jsx
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function Bucketlist() {
+  return (
+    <Container>
+      <h1 className="my-3">Bucket list</h1>
+    </Container>
+  )
+}
