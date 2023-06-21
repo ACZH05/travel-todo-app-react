40242a344f01c7cfe713ce344553d9d1b53f8eb1
Alfred Chin <alfredchin2005@gmail.com>
2023-06-21 11:59:10 +0800

    Add register form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,9 @@
 import { Button, Container, Form } from "react-bootstrap";
 
-export default function Login() {
+export default function Register() {
   return (
     <Container className="position-absolute top-50 start-50 translate-middle border border-2 p-5 rounded" style={{ width: "30%" }}>
-      <h1 className="mb-3">Login</h1>
+      <h1 className="mb-3">Register</h1>
       <Form.Group className="mb-4">
         <Form.Label>Username</Form.Label>
         <Form.Control type="text" placeholder="Enter username" required />
@@ -13,7 +13,7 @@ export default function Login() {
         <Form.Control type="password" placeholder="Enter password" required />
       </Form.Group>
       <Button className="mb-3 py-2" style={{ width: "100%"}}>Login</Button>
-      <p className="text-center"><a href="/register">First time using this app? Register here!</a></p>
+      <p className="text-center"><a href="/login">Got an account? Log in here!</a></p>
     </Container>
   )
 }
