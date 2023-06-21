30f51bfc9be13d668c7ba2c79bf309bbd704d7ff
Alfred Chin <alfredchin2005@gmail.com>
2023-06-21 19:25:55 +0800

    Display checklist

--- src/pages/Checklist.jsx
+++ src/pages/Checklist.jsx
@@ -1,4 +1,18 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
+import { checklist } from "../checklist";
+
+function ChecklistData({ checklist }) {
+    return checklist.checklist.map((list, index) => {
+        return (
+            <tr key={index}>
+                <td className="text-center">{index+1}</td>
+                <td>{list.item}</td>
+                <td className="text-center"><Form.Check type="checkbox" /></td>
+                <td className="text-center"><Form.Check type="checkbox" /></td>
+            </tr>
+        )
+    })
+}
 
 export default function Checklist() {
   return (
@@ -13,7 +27,9 @@ export default function Checklist() {
                     <th className="col-sm-2">Before leaving hotel</th>
                 </tr>
             </thead>
-            <tbody className="text-start"></tbody>
+            <tbody className="text-start">
+                <ChecklistData checklist={checklist} />
+            </tbody>
         </table>
     <Button className="position-absolute sticky-bottom end-0 m-5 px-4 py-2"><i className="bi bi-pencil-square me-2" />Edit</Button>
     </Container>
