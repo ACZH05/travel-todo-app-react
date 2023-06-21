b0f318042bfbc85ab44e307346436f38863fbb31
Alfred Chin <alfredchin2005@gmail.com>
2023-06-21 14:07:34 +0800

    Add checklist template

---
+++ src/checklist.js
@@ -0,0 +1,41 @@
+export const checklist = {
+    username: "Alfred",
+    checklist: [
+        {
+          item: "Passport",
+          homeCheckStatus: false,
+          hotelCheckStatus: false
+        },
+        {
+          item: "Boarding Pass",
+          homeCheckStatus: false,
+          hotelCheckStatus: false,
+        },
+        {
+          item: "ID Card",
+          homeCheckStatus: false,
+          hotelCheckStatus: false,
+        },
+        {
+          item: "Cash/Credit Cards",
+          homeCheckStatus: false,
+          hotelCheckStatus: false,
+        },
+        {
+          item: "Phone Charger",
+          homeCheckStatus: false,
+          hotelCheckStatus: false,
+        },
+        {
+          item: "Travel Itinerary",
+          homeCheckStatus: false,
+          hotelCheckStatus: false,
+        },
+        {
+          item: "Clothing",
+          homeCheckStatus: false,
+          hotelCheckStatus: false,
+        },
+    ],
+    bucketlist: []
+}
