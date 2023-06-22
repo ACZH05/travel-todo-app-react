a65638f6e34468a3f22c8c84fd3edd5d51bcd4b3
Alfred Chin <alfredchin2005@gmail.com>
2023-06-22 21:54:30 +0800

    Add log out function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Button, Container, Nav, Navbar } from 'react-bootstrap'
 import Checklist from './pages/Checklist'
 import Bucketlist from './pages/Bucketlist'
 import ErrorPage from './pages/ErrorPage'
@@ -11,18 +11,19 @@ import RequireAuth from './components/RequireAuth'
 import SectionAuth from './components/SectionAuth'
 import { UsersContext } from './Contexts/usersContext'
 
-function Layout() {
+function Layout({ setToken }) {
   return (
     <>
       <Navbar bg='light' variant='light'>
         <Container>
           <Navbar.Brand>Travel Plan App</Navbar.Brand>
-          <Nav className='me-auto'>
-            <SectionAuth>
-              <Nav.Link href='/'>Checklist</Nav.Link>
-              <Nav.Link href='/bucketlist'>Bucket list</Nav.Link>
-            </SectionAuth>
-          </Nav>
+          <SectionAuth>
+            <Nav className='me-auto'>
+                <Nav.Link href='/'>Checklist</Nav.Link>
+                <Nav.Link href='/bucketlist'>Bucket list</Nav.Link>
+            </Nav>
+            <Button variant='outline-dark' onClick={() => setToken(null)}>Logout</Button>
+          </SectionAuth>
         </Container>
       </Navbar>
       <Outlet />
@@ -38,7 +39,7 @@ export default function App() {
       <UsersContext.Provider value={{ users, setUsers}}>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Layout setToken={setToken} />}>
               <Route index element={<RequireAuth><Checklist /></RequireAuth>} />
               <Route path='bucketlist' element={<RequireAuth><Bucketlist /></RequireAuth>} />
               <Route path='login' element={<Login />} />
